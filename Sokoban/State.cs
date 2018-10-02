92c7e86614c4893bda566c77708af3f35724a5b0
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2018-10-02 08:05:48 +0700

    Add class State

---
+++ Sokoban/State.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Sokoban
+{
+	public class State : ICloneable
+	{
+		public Field Field { get; set; }
+		
+		public Coords PlayerCoords { get; set; }
+		public Coords[] BoxesCoords { get; set; }
+		
+		public State(Field field,
+		             Coords playerCoords, Coords[] boxesCoords)
+		{
+			Field = field;
+			PlayerCoords = playerCoords;
+			BoxesCoords = (Coords[]) boxesCoords.Clone();
+		}
+		
+		public bool Validate()
+		{
+			if (! Field.IsInBounds(PlayerCoords)) return false;
+			if (Field.GetCellAt(PlayerCoords) == Field.Cell.Wall) {
+				return false;
+			}
+			
+			for (int i = 0; i < BoxesCoords.Length; i++) {
+				if (! Field.IsInBounds(BoxesCoords[i])) return false;
+				if (Field.GetCellAt(BoxesCoords[i]) == Field.Cell.Wall) {
+					return false;
+				}
+				if (BoxesCoords[i] == PlayerCoords) return false;
+				for (int j = i + 1; j < BoxesCoords.Length; j++) {
+					if (BoxesCoords[i] == BoxesCoords[j]) return false;
+				}
+			}
+			
+			return true;
+		}
+		
+		public object Clone()
+		{
+			return new State(Field,
+			                 PlayerCoords, (Coords[])BoxesCoords.Clone());
+		}
+		
+		public override bool Equals(object obj)
+		{
+			State other = obj as State;
+			if (other == null) return false;
+			
+			if (this.PlayerCoords != other.PlayerCoords) return false;
+			if (this.BoxesCoords.Length != other.BoxesCoords.Length) return false;
+			for (int i = 0; i < this.BoxesCoords.Length; i++) {
+				if (this.BoxesCoords[i] != other.BoxesCoords[i]) return false;
+			}
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			int hashCode = 0;
+			unchecked {
+				hashCode += 1000000007 * PlayerCoords.GetHashCode();
+				if (BoxesCoords != null) {
+					foreach (var boxCoords in BoxesCoords) {
+						hashCode = 1000000009*hashCode + boxCoords.GetHashCode();
+					}
+				}
+			}
+			return hashCode;
+		}
+	}
+}
---
+++ Sokoban/Tests/StateTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sokoban.Tests
+{
+	[TestFixture]
+	public class StateTest
+	{
+		private Field field = new Field(
+			new Field.Cell[,] {
+				{ Field.Cell.Empty, Field.Cell.Target, Field.Cell.Wall },
+				{ Field.Cell.Empty, Field.Cell.Target, Field.Cell.Wall },
+			}
+		);
+		
+		private Coords validPlayerCoords = new Coords(1, 1);
+		private Coords invalidPlayerCoords = new Coords(0, 2);
+		
+		private Coords[] validBoxesCoords = new Coords[] {
+			new Coords(0, 0), new Coords(0, 1),
+		};
+		private Coords[] invalidBoxesCoords = new Coords[] {
+			new Coords(0, 0), new Coords(1, 2),
+		};
+		
+		[Test]
+		public void TestEquality()
+		{
+			var s1 = new State(field, validPlayerCoords, validBoxesCoords);
+			var s2 = new State(field, validPlayerCoords, validBoxesCoords);
+			var t1 = new State(field, invalidPlayerCoords, validBoxesCoords);
+			var t2 = new State(field, validPlayerCoords, invalidBoxesCoords);
+			
+			Assert.That(s1, Is.EqualTo(s2));
+			Assert.That(s1, Is.Not.EqualTo(t1));
+			Assert.That(s1, Is.Not.EqualTo(t2));
+			
+			Assert.That(s1.GetHashCode(), Is.EqualTo(s2.GetHashCode()));
+		}
+		
+		[Test]
+		public void TestCloning()
+		{
+			var s1 = new State(field, validPlayerCoords, validBoxesCoords);
+			var s2 = (State) s1.Clone();
+			
+			Assert.That(s1, Is.EqualTo(s2));
+			Assert.That(s1, Is.Not.SameAs(s2));
+			
+			Assert.That(s1.Field, Is.SameAs(s2.Field));
+		}
+		
+		[Test]
+		public void TestValidation()
+		{
+			Assert.That(new State(field, validPlayerCoords, validBoxesCoords)
+			            .Validate());
+			
+			Assert.That(! new State(field, invalidPlayerCoords, validBoxesCoords)
+			            .Validate());
+			Assert.That(! new State(field, validPlayerCoords, invalidBoxesCoords)
+			            .Validate());
+			
+			Assert.That(! new State(field, validBoxesCoords[0], validBoxesCoords)
+			            .Validate());
+		}
+	}
+}
