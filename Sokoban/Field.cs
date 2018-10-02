46676b3e04c324721d2e05988debcb59d25ee778
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2018-10-02 07:40:11 +0700

    Add class Field

---
+++ Sokoban/Field.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sokoban
+{
+	public class Field
+	{
+		public enum Cell { Empty, Wall, Target }
+		
+		private readonly Cell[,] cells;
+		
+		public Field(Cell[,] cells)
+		{
+			this.cells = (Cell[,]) cells.Clone();
+		}
+		
+		public Cell GetCellAt(Coords coords)
+		{
+			return this.cells[coords.Row, coords.Col];
+		}
+		
+		public int Height { get { return cells.GetLength(0); } }
+		public int Width { get { return cells.GetLength(1); } }
+		
+		public bool IsInBounds(Coords coords)
+		{
+			if (coords.Row < 0) return false;
+			if (coords.Row >= Height) return false;
+			if (coords.Col < 0) return false;
+			if (coords.Col >= Width) return false;
+			return true;
+		}
+	}
+}
---
+++ Sokoban/Tests/FieldTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sokoban.Tests
+{
+	[TestFixture]
+	public class FieldTest
+	{
+		private static Field.Cell[,] cells = new Field.Cell[,] {
+			{ Field.Cell.Empty, Field.Cell.Target, Field.Cell.Wall },
+			{ Field.Cell.Empty, Field.Cell.Target, Field.Cell.Wall },
+		};
+		
+		private Field field = new Field(cells);
+		
+		[Test]
+		public void TestBounds()
+		{
+			Assert.That(field.Width, Is.EqualTo(3));
+			Assert.That(field.Height, Is.EqualTo(2));
+			
+			Assert.That(field.IsInBounds(new Coords(1, 1)));
+			Assert.That(! field.IsInBounds(new Coords(1, 3)));
+			Assert.That(! field.IsInBounds(new Coords(0, -1)));
+		}
+		
+		[Test]
+		public void TestAccess()
+		{
+			Assert.That(field.GetCellAt(new Coords(1,1)),
+			            Is.EqualTo(Field.Cell.Target));
+			Assert.Throws(typeof(IndexOutOfRangeException),
+			              () => field.GetCellAt(new Coords(2,1)));
+		}
+	}
+}
