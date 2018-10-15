db1c634151717995505d880cc49ec543a2b11e84
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2018-10-16 04:52:22 +0700

    Add simple GUI implementation

--- SokobanGui/GameSessionWindow.xaml.cs
+++ SokobanGui/GameSessionWindow.xaml.cs
@@ -12,11 +12,68 @@ namespace SokobanGui
 {
 	public partial class GameSessionWindow : Window
 	{
+		public GameSession Session { get; private set; }
+		
 		public GameSessionWindow(GameSession session)
 		{
+			Session = session;
 			InitializeComponent();
-			
-			this.DataContext = session;
+			DataContext = session;
+		}
+		
+		void MoveUpCommandBinding_CanExecute(object sender,
+		                                     CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = Session.CanMoveUp;
+		}
+		void MoveUpCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			Session.MoveUp();
+		}
+		void MoveDownCommandBinding_CanExecute(object sender,
+		                                     CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = Session.CanMoveDown;
+		}
+		void MoveDownCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			Session.MoveDown();
+		}
+		void MoveLeftCommandBinding_CanExecute(object sender,
+		                                     CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = Session.CanMoveLeft;
+		}
+		void MoveLeftCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			Session.MoveLeft();
+		}
+		void MoveRightCommandBinding_CanExecute(object sender,
+		                                     CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = Session.CanMoveRight;
+		}
+		void MoveRightCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			Session.MoveRight();
+		}
+		void UndoCommandBinding_CanExecute(object sender,
+		                                     CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = Session.CanUndo;
+		}
+		void UndoCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			Session.Undo();
+		}
+		void RedoCommandBinding_CanExecute(object sender,
+		                                     CanExecuteRoutedEventArgs e)
+		{
+			e.CanExecute = Session.CanRedo;
+		}
+		void RedoCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			Session.Redo();
 		}
 	}
 }
